b216aa63618d1427939257d70492ac5f9a9bf7fd
조은준 <129740695+Sentimentaleunjun@users.noreply.github.com>
2025-09-03 22:57:28 +0900

    Update main.py

--- takkari-bot/main.py
+++ takkari-bot/main.py
@@ -6,16 +6,16 @@ from discord.ext import commands
 TOKEN = os.getenv("DISCORD_TOKEN")
 
 intents = discord.Intents.default()
-intents.message_content = True  # 메시지 내용 읽기 허용 (필요 시)
+intents.message_content = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 async def main():
     async with bot:
-        
-   for filename in os.listdir("./cogs"):
+        # Cogs 비동기 로딩
+        for filename in os.listdir("./cogs"):
             if filename.endswith(".py") and filename != "__init__.py":
                 await bot.load_extension(f"cogs.{filename[:-3]}")
-    
+        # 봇 실행
         await bot.start(TOKEN)
 
 if __name__ == "__main__":
