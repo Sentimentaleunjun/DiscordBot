70fad2bf6430904c0dd12ff58b6d8b650e8cdd95
조은준 <129740695+Sentimentaleunjun@users.noreply.github.com>
2025-09-05 21:18:02 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,19 +4,16 @@ import discord
 from discord.ext import commands
 from utils.db import init_db
 
-from support-web.app import app  # 웹서비스 가져오기
+from support_web.app import app 
 import threading
 import uvicorn
 
-# 환경변수
 TOKEN = os.getenv("DISCORD_TOKEN")
 DEV_ID = int(os.getenv("DEV_ID", "0"))
 
-# Discord Intents
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="/", intents=intents)
 
-# DB 초기화
 init_db()
 
 @bot.event
@@ -24,7 +21,7 @@ async def on_ready():
     print(f"✅ 봇 로그인 완료: {bot.user} (ID: {bot.user.id})")
 
 async def start_bot():
-    # cogs 로드
+    
     for filename in os.listdir("./takkari-bot/cogs"):
         if filename.endswith(".py") and filename != "__init__.py":
             try:
@@ -41,8 +38,5 @@ def start_web():
     uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 10000)))
 
 if __name__ == "__main__":
-    # 웹서버를 별도 스레드로 실행
     threading.Thread(target=start_web, daemon=True).start()
-
-    # 비동기 루프에서 디스코드 봇 실행
     asyncio.run(start_bot())
