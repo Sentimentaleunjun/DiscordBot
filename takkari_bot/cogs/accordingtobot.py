016cb06a82ca39838c5a22dcb184d959159dee4a
조은준 <129740695+Sentimentaleunjun@users.noreply.github.com>
2025-09-06 17:53:02 +0900

    Update accordingtobot.py

--- takkari_bot/cogs/accordingtobot.py
+++ takkari_bot/cogs/accordingtobot.py
@@ -8,7 +8,7 @@ class AccordingToBot(commands.Cog):
 
     @app_commands.command(name="accordingtobot", description="따까리봇 설명")
     async def accordingtobot(self, interaction: discord.Interaction):
-        await interaction.response.send_message("🤖 따까리봇은 서버 관리를 도와주는 디스코드 봇입니다!")
+        await interaction.response.send_message("🤖 따까리봇은 서버 관리를 도와주는 디스코드 봇입니다!", ephemeral=True)
 
 async def setup(bot):
     await bot.add_cog(AccordingToBot(bot))
