ee100ea6eaad850e50b75d16b535720ed7e8ee75
조은준 <129740695+Sentimentaleunjun@users.noreply.github.com>
2025-09-06 10:06:17 +0900

    Update db_lookup.py

--- takkari_bot/cogs/db_lookup.py
+++ takkari_bot/cogs/db_lookup.py
@@ -1,22 +1,25 @@
-﻿import discord
+# -*- coding: utf-8 -*-
+import discord
 from discord.ext import commands
 from takkari_bot.utils.db import get_supports
 
-class DBLookup(commands.Cog):
+class DBLookupCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="dblookup")
-    async def dblookup(self, ctx):
-        if not ctx.author.guild_permissions.administrator:
-            await ctx.send("??愿由ъ옄留?媛?ν빀?덈떎.")
-            return
+    @commands.command(name="지원목록")
+    async def lookup(self, ctx):
+        """DB에서 지원자 목록 불러오기"""
         supports = get_supports()
         if not supports:
-            await ctx.send("?벊 臾몄쓽媛 ?놁뒿?덈떎.")
-        else:
-            msg = "\n".join([f"{u}: {m}" for u, m in supports[:10]])
-            await ctx.send(f"?뱦 理쒓렐 臾몄쓽:\n{msg}")
+            await ctx.send("지원자가 없습니다.")
+            return
+        embed = discord.Embed(
+            title="지원자 목록",
+            description="\n".join(supports),
+            color=0xffcc00
+        )
+        await ctx.send(embed=embed)
 
 async def setup(bot):
-    await bot.add_cog(DBLookup(bot))
+    await bot.add_cog(DBLookupCog(bot))
