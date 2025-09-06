4913c69c4fbf6524d9e9f38aa9fe288fbffe7119
조은준 <129740695+Sentimentaleunjun@users.noreply.github.com>
2025-09-06 10:06:05 +0900

    Update help.py

--- takkari_bot/cogs/help.py
+++ takkari_bot/cogs/help.py
@@ -1,20 +1,22 @@
-﻿import discord
+# -*- coding: utf-8 -*-
+import discord
 from discord.ext import commands
 
-class Help(commands.Cog):
+class HelpCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="help")
-    async def help(self, ctx):
-        embed = discord.Embed(title="?뱰 ?꾩?留?, description="?ъ슜 媛?ν븳 紐낅졊??紐⑸줉?낅땲??", color=0x00ffcc)
-        embed.add_field(name="/help", value="?꾩?留??뺤씤", inline=False)
-        embed.add_field(name="/patchnote", value="?⑥튂?명듃 ?뺤씤/異붽? (愿由ъ옄留?異붽? 媛??", inline=False)
-        embed.add_field(name="/support [?댁슜]", value="怨좉컼吏??臾몄쓽 ?깅줉", inline=False)
-        embed.add_field(name="/dblookup", value="DB 議고쉶 (愿由ъ옄 ?꾩슜)", inline=False)
-        embed.add_field(name="/accordingtobot [?댁슜]", value="怨듭? 諛쒖넚 (愿由ъ옄 ?꾩슜)", inline=False)
-        embed.add_field(name="/schedule [YYYY-MM-DD HH:MM] [?댁슜]", value="怨듭? ?덉빟 (愿由ъ옄 ?꾩슜)", inline=False)
+    @commands.command(name="도움말")
+    async def help_command(self, ctx):
+        embed = discord.Embed(
+            title="따까리봇 도움말",
+            description="사용 가능한 명령어입니다.",
+            color=0x00ffcc
+        )
+        embed.add_field(name="!지원 <메시지>", value="지원 메시지 남기기", inline=False)
+        embed.add_field(name="!예약공지 <초> <메시지>", value="예약 공지 등록", inline=False)
+        embed.add_field(name="!패치노트", value="최근 패치노트 확인", inline=False)
         await ctx.send(embed=embed)
 
 async def setup(bot):
-    await bot.add_cog(Help(bot))
+    await bot.add_cog(HelpCog(bot))
