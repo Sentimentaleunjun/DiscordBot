0ad3df01ba324dd554ec69d5e86c3410ace2df34
조은준 <129740695+Sentimentaleunjun@users.noreply.github.com>
2025-09-06 10:06:43 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,10 @@
-﻿import os
-from threading import Thread
-from takkari_bot.main_bot import run_discord_bot  
-
-from support_web.app import app  
-
-
-def run_flask():
-    port = int(os.environ.get("PORT", 5000))  # Render媛 PORT ?먮룞 ?좊떦
-    app.run(host="0.0.0.0", port=port)
-
+# -*- coding: utf-8 -*-
+from takkari_bot.main_bot import run_discord_bot
+from support_web.app import run_web
 
 def run_bot():
     run_discord_bot()
 
-
 if __name__ == "__main__":
-    
-    flask_thread = Thread(target=run_flask, daemon=True)
-    flask_thread.start()
-
-    
+    run_web()
     run_bot()
