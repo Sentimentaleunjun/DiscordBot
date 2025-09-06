b9d8b51b738fbfb7f2a824719f11411fde8d613c
조은준 <129740695+Sentimentaleunjun@users.noreply.github.com>
2025-09-06 14:30:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
+# main.py
 # -*- coding: utf-8 -*-
 import threading
-from takkari_bot.main_bot import run_discord_bot
 from support_web.app import run_web
+from takkari_bot.main_bot import run_discord_bot
 
 def run_bot():
     run_discord_bot()
 
-def run_flask():
-    run_web()
-
 if __name__ == "__main__":
-    # 웹 서버와 디스코드 봇을 동시에 실행
-    t1 = threading.Thread(target=run_flask)
-    t1.start()
+    # Flask 웹 서버를 별도 스레드에서 실행
+    t = threading.Thread(target=run_web)
+    t.start()
 
+    # Discord 봇 실행
     run_bot()
