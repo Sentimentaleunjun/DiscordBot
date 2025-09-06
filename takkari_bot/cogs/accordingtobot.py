4dbb9ff29f24dc59ffa11ec5e1a06fb5adf634fd
조은준 <129740695+Sentimentaleunjun@users.noreply.github.com>
2025-09-06 14:51:32 +0900

    Update accordingtobot.py

--- takkari_bot/cogs/accordingtobot.py
+++ takkari_bot/cogs/accordingtobot.py
@@ -1,20 +1,14 @@
-# -*- coding: utf-8 -*-
 import discord
+from discord import app_commands
 from discord.ext import commands
 
-class AccordingToBotCog(commands.Cog):
+class AccordingToBot(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="따까리봇")
-    async def info(self, ctx):
-        embed = discord.Embed(
-            title="따까리봇 정보",
-            description="나는 주인님의 충직한 따까리봇입니다 🤖",
-            color=0xff66cc
-        )
-        embed.add_field(name="기능", value="공지, 예약, 지원, 패치노트, DB 조회", inline=False)
-        await ctx.send(embed=embed)
+    @app_commands.command(name="according_to_bot", description="봇 상태를 확인합니다.")
+    async def according_to_bot(self, interaction: discord.Interaction):
+        await interaction.response.send_message("🤖 봇이 정상적으로 동작 중입니다!")
 
 async def setup(bot):
-    await bot.add_cog(AccordingToBotCog(bot))
+    await bot.add_cog(AccordingToBot(bot))
