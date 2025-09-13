4da78c74bc0793652775eb6e2fe17c22bf5c95f5
조은준 <129740695+Sentimentaleunjun@users.noreply.github.com>
2025-09-13 23:42:16 +0900

    Update and rename db_lookup.py to dblookup.py

--- takkari_bot/cogs/dblookup.py
+++ takkari_bot/cogs/dblookup.py
@@ -6,6 +6,9 @@ from discord.ext import commands
 class DBLookup(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        
+     def cog_load(self):
+        self.bot.tree.add_command(self.dblookup)
 
     @app_commands.command(name="dblookup", description="DB를 조회합니다.")
     async def db_lookup(self, interaction: discord.Interaction, key: str):
