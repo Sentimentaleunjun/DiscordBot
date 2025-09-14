67550183049f9288426c3d9993f786ac727090d8
조은준 <129740695+Sentimentaleunjun@users.noreply.github.com>
2025-09-14 20:10:20 +0900

    Update riot.py

--- takkari_bot/cogs/riot.py
+++ takkari_bot/cogs/riot.py
@@ -12,7 +12,7 @@ class RiotOAuth(commands.Cog):
         client_id = os.environ.get("RIOT_CLIENT_ID")
         redirect_uri = os.environ.get("RIOT_REDIRECT_URI")
         if not client_id or not redirect_uri:
-            await interaction.response.send_message("TKR 001 오류 . /support나 gsej support 사이트에서 오류코드를 검색하세요", ephemeral=True)
+            await interaction.response.send_message("TKR 001 오류 . /support로 관리자에게 문의하세요", ephemeral=True)
             return
 
         oauth_url = (
